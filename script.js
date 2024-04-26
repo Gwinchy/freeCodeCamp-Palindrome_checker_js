f68e51e72a933554be8b8b8c5127e0d49d6c1b0a
Gwinchy <gmunyui254@gmail.com>
2024-04-27 00:32:11 +0300

    cleaned and polished

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ const checkButton = document.getElementById("check-btn");
 const inputText = document.getElementById('text-input');
 const result = document.getElementById("result");
 
-   checkButton.addEventListener("click", () => {
-     if (inputText.value === "") {
-       alert("Please input a value");
-     }
-     else if (inputText.value.length = 1) {
-       result.innerText = inputText.value + " is a palindrome";
-     }
-const cleanText = inputText.toLowerCase().replace(/[^a-z0-9]/g, "");
-if (cleanText === [...cleanText].reverse().join("")) {
-  result.innerText = inputText.value + " is a palindrome";
-}
-result.innerText = inputText.value + " is not a palindrome";
-})
+checkButton.addEventListener("click", () => {
+  if (inputText.value === "") {
+    alert("Please input a value");
+  } else {
+    const cleanText = inputText.value.toLowerCase().replace(/[^a-z0-9]/g, "");
+    if (cleanText === [...cleanText].reverse().join("")) {
+      result.innerText = inputText.value + " is a palindrome";
+    } else {
+      result.innerText = inputText.value + " is not a palindrome";
+    }
+  }
+});
